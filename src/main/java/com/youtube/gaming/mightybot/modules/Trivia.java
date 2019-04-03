ee9cddb525daf02a9514fc883722c7b0ff495ac1
Emmanuel Cron <emmanuel.cron@gmail.com>
2019-04-03 01:32:57 -0700

    Now reading from a serialized database.

--- src/main/java/com/youtube/gaming/mightybot/modules/Trivia.java
+++ src/main/java/com/youtube/gaming/mightybot/modules/Trivia.java
@@ -1,13 +1,11 @@
 package com.youtube.gaming.mightybot.modules;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -21,11 +19,7 @@ import com.google.common.collect.ImmutableSet;
 import com.youtube.gaming.mightybot.MightyContext;
 import com.youtube.gaming.mightybot.Module;
 import com.youtube.gaming.mightybot.exceptions.InvalidConfigurationException;
-import com.youtube.gaming.mightybot.modules.trivia.BooleanTriviaQuestion;
-import com.youtube.gaming.mightybot.modules.trivia.GuessTriviaQuestion;
-import com.youtube.gaming.mightybot.modules.trivia.MultipleChoicesTriviaQuestion;
 import com.youtube.gaming.mightybot.modules.trivia.TriviaQuestion;
-import com.youtube.gaming.mightybot.modules.trivia.TriviaQuestionType;
 import com.youtube.gaming.mightybot.util.DynamicPath;
 
 /**
@@ -61,15 +55,9 @@ public class Trivia extends Module {
     currentQuestionIndex = -1;
     triviaQuestions = new ArrayList<TriviaQuestion>();
 
-    try (InputStream input = new FileInputStream(dbPath.toAbsolutePath().toFile());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"))) {
-      readDatabase(reader);
-    } catch (IOException e) {
-      logger.error("Could not Trivia DB", e);
-    }
+    triviaQuestions = readDatabase(dbPath);
 
-    logger.info("Read {} questions Trivia DB from: {}", triviaQuestions.size(),
-        dbPath.toAbsolutePath());
+    logger.info("Read {} questions.", triviaQuestions.size());
   }
 
   @Override
@@ -93,67 +81,21 @@ public class Trivia extends Module {
     }
   }
 
-  public void readDatabase(BufferedReader reader) throws IOException {
-    String line;
-    while ((line = reader.readLine()) != null) {
-      if (line.isEmpty() || line.startsWith("#")) {
-        continue;
-      }
+  public List<TriviaQuestion> readDatabase(Path dbPath) {
+    logger.info("Reading Trivia database from: {}", dbPath.toAbsolutePath());
 
-      if (line.equalsIgnoreCase(TriviaQuestionType.BOOLEAN.name())) {
-        String question = assertStringNonEmpty(reader.readLine(), "boolean question is missing");
-        Boolean answer = assertStringBoolean(reader.readLine());
-        triviaQuestions.add(new BooleanTriviaQuestion(question, answer));
-      } else if (line.equalsIgnoreCase(TriviaQuestionType.MULTIPLE_CHOICES.name())) {
-        String question =
-            assertStringNonEmpty(reader.readLine(), "multiple choices question is missing");
-        String correctAnswer =
-            assertStringNonEmpty(reader.readLine(),
-                "multiple choices question correct answer is missing");
-        List<String> incorrectAnswers = new ArrayList<String>();
-        // At least one incorrect answer must be provided
-        incorrectAnswers.add(assertStringNonEmpty(reader.readLine(),
-            "multiple choices question has no incorrect answer provided"));
-        while ((line = reader.readLine()) != null) {
-          if (line.isEmpty()) {
-            break;
-          }
-          incorrectAnswers.add(line);
-        }
-        triviaQuestions
-            .add(new MultipleChoicesTriviaQuestion(question, correctAnswer, incorrectAnswers));
-      } else if (line.equalsIgnoreCase(TriviaQuestionType.GUESS.name())) {
-        String question = assertStringNonEmpty(reader.readLine(), "guess question is missing");
-        List<String> correctAnswers = new ArrayList<String>();
-        // At least one correct answer must be provided
-        correctAnswers.add(assertStringNonEmpty(reader.readLine(),
-            "guess question has no correct answer provided"));
-        while ((line = reader.readLine()) != null) {
-          if (line.isEmpty()) {
-            break;
-          }
-          correctAnswers.add(line);
-        }
-        triviaQuestions.add(new GuessTriviaQuestion(question, correctAnswers));
-      }
+    try (InputStream inputStream = new FileInputStream(dbPath.toAbsolutePath().toFile());
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+      @SuppressWarnings("unchecked")
+      List<TriviaQuestion> triviaQuestions = (List<TriviaQuestion>) objectInputStream.readObject();
+      return triviaQuestions;
+    } catch (ClassNotFoundException cnfe) {
+      logger.error("Could not parse Trivia DB", cnfe);
+      throw new RuntimeException();
+    } catch (IOException e) {
+      logger.error("Could not read Trivia DB", e);
+      throw new RuntimeException();
     }
-
-    Collections.shuffle(triviaQuestions);
-  }
-
-  public String assertStringNonEmpty(String str, String errorMsg) {
-    if (str == null || str.isEmpty()) {
-      throw new RuntimeException("Expected string, but got null or empty: " + errorMsg);
-    }
-    return str;
-  }
-
-  public Boolean assertStringBoolean(String str) {
-    assertStringNonEmpty(str, "'true' or 'false' expected");
-    if (!"true".equalsIgnoreCase(str) && !"false".equalsIgnoreCase(str)) {
-      throw new RuntimeException("Expected 'true' or 'false', but found: " + str);
-    }
-    return Boolean.valueOf(str);
   }
 
 //  public void postQuestion() {
